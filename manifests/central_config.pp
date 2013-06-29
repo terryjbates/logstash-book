7d5f2d9d21fa7e18559921e5617e7892d7d29426
Terry Bates <terryjbates@gmail.com>
2013-06-28 21:10:47 -0400

    Modified central_config.pp to make the logstash-central service require that Redis and ElasticSearch be running.

--- manifests/central_config.pp
+++ manifests/central_config.pp
@@ -120,11 +120,12 @@ file { "/etc/init.d/logstash-central":
 }
 
 
-# Run the logstash-central init script.
+# Run the logstash-central init script. But only after we have 
+# the services we rely on up and running.
 service { "logstash-central":
   enable => "true",
   ensure => "running",
-  require => File[ "/etc/init.d/logstash-central" ],
+  require => [ Service[ "redis-server" ], Service[ "elasticsearch" ]  ],
 }
 
 
